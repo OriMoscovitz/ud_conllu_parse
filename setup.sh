4d682ff655ef54fab32c3f7c9663937f70d57278
OriMoscovitz <Ororior208@gmail.com>
2026-04-12 14:25:10 +0200

    update venv creation

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-echo "Creating virtual environment..."
-python -m venv .venv
+# Create virtual environment if it doesn't exist
+if [ ! -d ".venv" ]; then
+  echo "Creating virtual environment..."
+  python -m venv .venv
+else
+  echo "Virtual environment already exists."
+fi
 
 echo "Activating virtual environment..."
 source .venv/bin/activate
